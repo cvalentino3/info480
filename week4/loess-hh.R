b5a55fbe6a4d6818d83a4546ec788a0cd81060fa
Carmine Valentino <cvalentino@blacklabelsoft.com>
2013-05-14 19:38:31 -0400

    Edits for Project Proposal

--- week4/loess-hh.R
+++ week4/loess-hh.R
@@ -1,11 +1,11 @@
-mydata <-read.csv("http://ds101.seangoggins.net/Example-ND.csv", sep=",")
+mydata <-read.csv("http://enigma.blacklabelsoft.com/public/InternetTraffic.csv", sep=",")
 attach(mydata)
 
 #Plots the data
-plot(Year, EMD)
+plot(Year, IP_Traffic)
 
 # Apply loess smoothing using the default span value of 0.8.  You can change the curve by changing the span value.
-y.loess <- loess(y ~ x, span=0.8, data.frame(x=Year, y=EMD))
+y.loess <- loess(y ~ x, span=0.8, data.frame(x=Year, y=IP_Traffic))
 
 # Compute loess smoothed values for all points along the curve
 y.predict <- predict(y.loess, data.frame(x=Year))
@@ -13,5 +13,9 @@ y.predict <- predict(y.loess, data.frame(x=Year))
 # Plots the curve.
 lines(Year,y.predict)
 
+legend("topleft", c("PB/Month"), col = c(1), cex = 0.75,
+       text.col = "black", lty = c(1 ,-1, -1), pch = c(-1, 19, 17),
+       merge = TRUE, bg = 'gray90')
+
 #Add title
-title(main="Simple Code Example")
+title(main="Global Internet Traffic")
